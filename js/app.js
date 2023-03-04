1be27eb2309627a5b899cfc6c85e57f512e2515f
Nyashka-Enotik <89606553+Nyashka-Enotik@users.noreply.github.com>
2023-03-04 19:05:38 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,17 +6,61 @@ $('.img-contacts').slick({
     arrows: false,
 });
 
-
-
-$('.vivoz').each(function(i, e){
-    if ($(e).find('.vivoz__item').length > 1) {
-        $(e).slick({
+var contacts_slider = document.querySelectorAll(".contacts__items");
+for (var i = 0; i < contacts_slider.length; i++) {
+    var contacts_item = contacts_slider[i].querySelectorAll(".contacts__item");
+    if (contacts_item.length > 1) {
+        $(contacts_slider[i]).slick({
             slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 2500,
-            arrows: false,
+            autoplay: false,
+            speed: 500,
+            slidesToShow: 1,
+            dots: false,
+            fade: true,
+            prevArrow: '<button class="prev"></button>',
+            nextArrow: '<button class="next"></button>',
         });
     }
-})
+    else{
+        $(contacts_slider[i]).slick({
+            slidesToScroll: 1,
+            autoplay: false,
+            arrows: false,
+            speed: 500,
+            slidesToShow: 1,
+            dots: false,
+            fade: true,
+        });
+    }
+}
+var contacts_slider2 = document.querySelectorAll(".contacts__items2");
+for (var i = 0; i < contacts_slider2.length; i++) {
+    var contacts_item2 = contacts_slider2[i].querySelectorAll(".contacts__item");
+    if (contacts_item2.length > 1) {
+        $(contacts_slider2[i]).slick({
+            slidesToScroll: 1,
+            autoplay: false,
+            speed: 500,
+            slidesToShow: 1,
+            dots: false,
+            fade: true,
+            prevArrow: '<button class="prev2"></button>',
+            nextArrow: '<button class="next2"></button>',
+        });
+    }
+    else{
+        $(contacts_slider[i]).slick({
+            slidesToScroll: 1,
+            autoplay: false,
+            arrows: false,
+            speed: 500,
+            slidesToShow: 1,
+            dots: false,
+            fade: true,
+        });
+    }
+}
+
+
 
 
